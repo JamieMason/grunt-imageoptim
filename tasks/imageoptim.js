e94b04be05402e708f84487b57f48b29021ead7a
Adam Simpson <adam@adamsimpson.net>
2013-06-07 02:44:07 +0100

    feat(options): add support for --image-alpha and --quit

--- tasks/imageoptim.js
+++ tasks/imageoptim.js
@@ -12,10 +12,14 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('imageoptim', 'Losslessly compress images from the command line', function() {
 
+    var complete = 0;
     var directories = this.filesSrc;
     var done = this.async();
     var exec = require('child_process').exec;
-    var complete = 0;
+    var options = this.options({
+      imageAlpha: false,
+      quitAfter: false
+    });
 
     if (!directories.length) {
       grunt.fail.fatal('No valid directories were supplied for processing', 1);
@@ -23,11 +27,24 @@ module.exports = function(grunt) {
 
     directories.forEach(function(dir) {
 
+      var command;
       var imageOptim;
+      var imageAlpha = options.imageAlpha;
+      var quitAfter = options.quitAfter;
+
+      if (quitAfter && imageAlpha) {
+        command = 'imageOptim --quit --image-alpha --directory ';
+      } else if (!quitAfter && imageAlpha) {
+        command = 'imageOptim --image-alpha --directory ';
+      } else if (quitAfter && !imageAlpha) {
+        command = 'imageOptim --quit --directory ';
+      } else {
+        command = 'imageOptim --directory ';
+      }
 
       grunt.log.writeln('Processing "' + dir + '"');
 
-      imageOptim = exec('imageOptim -d ' + dir, function(error, stdout, stderr) {
+      imageOptim = exec(command + dir, function(error, stdout, stderr) {
         if (error !== null) {
           done(error);
         }
