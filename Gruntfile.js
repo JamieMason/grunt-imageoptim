299acd7c9e0eb760978f37c0f2c3c33b667dc005
Jamie Mason <siunmaru@gmail.com>
2013-06-08 14:52:50 +0100

    added options to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,11 @@ module.exports = function(grunt) {
 
     // Configuration to be run (and then tested).
     imageoptim: {
-      files: []
+      files: [],
+      options: {
+        imageAlpha: false,
+        quitAfter: true
+      }
     },
 
     // Unit tests.
