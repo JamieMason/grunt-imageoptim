f2f6da6f8a4d822c381911d5d1b4cf8c0e03b84c
Jamie Mason <siunmaru@gmail.com>
2013-08-04 11:03:10 +0100

    housekeeping

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,23 +6,12 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
-
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+  'use strict';
 
-    // Configuration to be run (and then tested).
+  grunt.initConfig({
+
     imageoptim: {
       files: [],
       options: {
@@ -30,26 +19,10 @@ module.exports = function(grunt) {
         imageAlpha: false,
         quitAfter: false
       }
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
     }
 
   });
 
-  // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Testing Task
-  grunt.registerTask('test', ['imageoptim', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
 
 };
