b8c7ebb75ea3cdd4bf86f76cec6e46ddd44e9687
Jamie Mason <siunmaru@gmail.com>
2013-08-04 13:03:40 +0100

    handle paths containing tildes, such as '~/Desktop/directory'

--- tasks/imageoptim.js
+++ tasks/imageoptim.js
@@ -29,15 +29,89 @@ module.exports = function(grunt) {
 
   'use strict';
 
+  var onTaskComplete;
+  var dirsTotal;
+  var dirsProcessed;
+
+  /**
+   * Extend http://nodejs.org/api/path.html to handle
+   * tildes in paths, such as '~/Desktop/directory'
+   * @param  {String} route
+   * @return {String}
+   */
+  function resolveTilde(route) {
+    if (route.substr(0, 1) === '~') {
+      route = process.env.HOME + route.substr(1);
+    }
+    return require('path').resolve(route);
+  }
+
+  /**
+   * A logging wrapper mainly to exclude stdout's we're getting which only contain whitespace
+   * @param  {String}  message
+   * @param  {Boolean} isError
+   */
+  function logMessage(message, isError) {
+
+    // quit if message is empty or only contains whitespace
+    if (!message || String(message).search(/\S/) === -1) {
+      return;
+    }
+
+    // remove trailing new lines
+    message = message.replace(/\n$/, '');
+
+    if (isError) {
+      grunt.fail.fatal(message);
+    } else {
+      grunt.log.writeln(message);
+    }
+  }
+
+  /**
+   * Handle stdout from ImageOptim-CLI
+   * @param  {Object|Null} error
+   * @param  {String} stdout
+   */
+  function onTerminalOutput(error, stdout) {
+    if (error !== null) {
+      logMessage(stdout, true);
+      onTaskComplete(error);
+    } else {
+      logMessage(stdout);
+    }
+  }
+
+  /**
+   * Based on the configured options, build up the ImageOptim-CLI Terminal command
+   * @param  {Object} options
+   * @return {String}
+   */
+  function createTerminalCommand(options) {
+    var command = './imageOptim';
+    if (options.quitAfter) { command += ' --quit'; }
+    if (options.imageAlpha) { command += ' --image-alpha'; }
+    if (options.jpegMini) { command += ' --jpeg-mini'; }
+    return command + ' --directory';
+  }
+
+  /**
+   * Called after we've finished processing each directory
+   * @param  {String} code
+   */
+  function onTerminalExit(code) {
+    if (++dirsProcessed === dirsTotal) {
+      onTaskComplete(code);
+    }
+  }
+
   grunt.registerMultiTask('imageoptim', 'Losslessly compress images from the command line', function() {
 
     var runTerminal = require('child_process').exec;
-    var toAbsolutePath = require('path').resolve;
-    var onTaskComplete = this.async();
+    var path = require('path');
+    var toAbsolutePath = path.resolve;
 
-    var dirs = this.filesSrc;
-    var dirsTotal = dirs.length;
-    var dirsProcessed = 0;
+    var dirs = this.data instanceof Array ? this.data.map(resolveTilde) : [];
 
     // locate the ImageOptim-CLI executable we have set as a local npm dependency
     var imageOptimCliPath = toAbsolutePath(__dirname, '../node_modules/imageoptim-cli/bin');
@@ -48,78 +122,26 @@ module.exports = function(grunt) {
       quitAfter: false
     });
 
-    // based on the options, build up the ImageOptim-CLI Terminal command
-    var terminalCommand = (function(options) {
-      var command = './imageOptim';
-      if (options.quitAfter) { command += ' --quit'; }
-      if (options.imageAlpha) { command += ' --image-alpha'; }
-      if (options.jpegMini) { command += ' --jpeg-mini'; }
-      return command + ' --directory';
-    }(options));
+    var terminalCommand = createTerminalCommand(options);
 
     var tplOnFile = '';
 
     tplOnFile += 'Psssst! ImageOptim-CLI uses folders. Pass the folder containing "{{fullPath}}" ';
     tplOnFile += 'instead and we should be good to go.';
 
-    /**
-     * A logging wrapper mainly to exclude stdout's we're getting which only contain whitespace
-     * @param  {String}  message
-     * @param  {Boolean} isError
-     */
-    function logMessage(message, isError) {
-
-      // quit if message is empty or only contains whitespace
-      if (!message || String(message).search(/\S/) === -1) {
-        return;
-      }
-
-      // remove trailing new lines
-      message = message.replace(/\n$/, '');
-
-      if (isError) {
-        grunt.fail.fatal(message);
-      } else {
-        grunt.log.writeln(message);
-      }
-    }
-
-    /**
-     * Handle stdout from ImageOptim-CLI
-     * @param  {Object|Null} error
-     * @param  {String} stdout
-     */
-    function onTerminalOutput(error, stdout) {
-      if (error !== null) {
-        logMessage(stdout, true);
-        onTaskComplete(error);
-      } else {
-        logMessage(stdout);
-      }
-    }
-
-    /**
-     * Called after we've finished processing each directory
-     * @param  {String} code
-     */
-    function onTerminalExit(code) {
-      if (++dirsProcessed === dirsTotal) {
-        onTaskComplete(code);
-      }
-    }
+    onTaskComplete = this.async();
+    dirsTotal = dirs.length;
+    dirsProcessed = 0;
 
     // if none of the glob patterns matched anything we've nothing to do
     if (!dirsTotal) {
       grunt.fail.fatal('No valid directories were supplied for processing', 1);
     }
 
-    dirs.forEach(function(imgPath) {
+    dirs.forEach(function(fullPath) {
 
       // a reference to the ImageOptim-CLI process running in the terminal
       var imageOptim;
-
-      // an absolute path to this folder
-      var fullPath = toAbsolutePath(imgPath);
 
       // the absolute path with whitespace escaped for the terminal
       var escapedFullPath = fullPath.replace(/\s/g, '\\ ');
