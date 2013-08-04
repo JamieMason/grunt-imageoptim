e7bad6770fa07b93d17ec6781e117a5f630f4a0a
Jamie Mason <siunmaru@gmail.com>
2013-08-04 17:31:24 +0100

    refactoring, improved error handling

--- tasks/imageoptim.js
+++ tasks/imageoptim.js
@@ -29,143 +29,140 @@ module.exports = function(grunt) {
 
   'use strict';
 
-  var onTaskComplete;
-  var dirsTotal;
-  var dirsProcessed;
+  var q = require('q');
+  var path = require('path');
+  var fs = require('fs');
+  var stat = q.denodeify(fs.stat);
+  var taskName = 'imageoptim';
+  var taskDescription = 'Losslessly compress images from the command line';
 
-  /**
-   * Extend http://nodejs.org/api/path.html to handle
-   * tildes in paths, such as '~/Desktop/directory'
-   * @param  {String} route
-   * @return {String}
-   */
-  function resolveTilde(route) {
-    if (route.substr(0, 1) === '~') {
-      route = process.env.HOME + route.substr(1);
-    }
-    return require('path').resolve(route);
-  }
-
-  /**
-   * A logging wrapper mainly to exclude stdout's we're getting which only contain whitespace
-   * @param  {String}  message
-   * @param  {Boolean} isError
-   */
-  function logMessage(message, isError) {
-
-    // quit if message is empty or only contains whitespace
-    if (!message || String(message).search(/\S/) === -1) {
-      return;
-    }
-
-    // remove trailing new lines
-    message = message.replace(/\n$/, '');
-
-    if (isError) {
-      grunt.fail.fatal(message);
-    } else {
-      grunt.log.writeln(message);
-    }
-  }
-
-  /**
-   * Handle stdout from ImageOptim-CLI
-   * @param  {Object|Null} error
-   * @param  {String} stdout
-   */
-  function onTerminalOutput(error, stdout) {
-    if (error !== null) {
-      logMessage(stdout, true);
-      onTaskComplete(error);
-    } else {
-      logMessage(stdout);
-    }
-  }
-
-  /**
-   * Based on the configured options, build up the ImageOptim-CLI Terminal command
-   * @param  {Object} options
-   * @return {String}
-   */
-  function createTerminalCommand(options) {
-    var command = './imageOptim';
-    if (options.quitAfter) { command += ' --quit'; }
-    if (options.imageAlpha) { command += ' --image-alpha'; }
-    if (options.jpegMini) { command += ' --jpeg-mini'; }
-    return command + ' --directory';
-  }
-
-  /**
-   * Called after we've finished processing each directory
-   * @param  {String} code
-   */
-  function onTerminalExit(code) {
-    if (++dirsProcessed === dirsTotal) {
-      onTaskComplete(code);
-    }
-  }
-
-  grunt.registerMultiTask('imageoptim', 'Losslessly compress images from the command line', function() {
-
-    var runTerminal = require('child_process').exec;
-    var path = require('path');
-    var toAbsolutePath = path.resolve;
-
-    var dirs = this.data instanceof Array ? this.data.map(resolveTilde) : [];
-
-    // locate the ImageOptim-CLI executable we have set as a local npm dependency
-    var imageOptimCliPath = toAbsolutePath(__dirname, '../node_modules/imageoptim-cli/bin');
-
-    var options = this.options({
-      jpegMini: false,
-      imageAlpha: false,
-      quitAfter: false
-    });
-
-    var terminalCommand = createTerminalCommand(options);
-
-    var tplOnFile = '';
-
-    tplOnFile += 'Psssst! ImageOptim-CLI uses folders. Pass the folder containing "{{fullPath}}" ';
-    tplOnFile += 'instead and we should be good to go.';
-
-    onTaskComplete = this.async();
-    dirsTotal = dirs.length;
-    dirsProcessed = 0;
-
-    // if none of the glob patterns matched anything we've nothing to do
-    if (!dirsTotal) {
-      grunt.fail.fatal('No valid directories were supplied for processing', 1);
-    }
-
-    dirs.forEach(function(fullPath) {
-
-      // a reference to the ImageOptim-CLI process running in the terminal
-      var imageOptim;
-
-      // the absolute path with whitespace escaped for the terminal
-      var escapedFullPath = fullPath.replace(/\s/g, '\\ ');
-
-      // apply the base ImageOptim-CLI command to this folder
-      var execImageOptim = terminalCommand + ' ' + escapedFullPath;
-
-      // update on progress
-      grunt.log.writeln('Processing "' + fullPath + '"');
-
-      // quit if the glob pattern matched anything other than folders
-      if (!grunt.file.isDir(fullPath)) {
-        grunt.fail.fatal(tplOnFile.replace('{{fullPath}}', fullPath), 1);
+  function fileExists(filePath) {
+    var deferred = q.defer();
+    fs.exists(filePath, function(canLocate) {
+      if (canLocate) {
+        deferred.resolve(true);
+      } else {
+        deferred.reject(new Error('"' + filePath + '" does not exist'));
       }
-
-      // run ImageOptim-CLI with the current working directory set to ImageOptim-CLI's bin folder
-      imageOptim = runTerminal(execImageOptim, {
-        cwd: imageOptimCliPath
-      }, onTerminalOutput);
-
-      // After we've processed the last folder we can quit
-      imageOptim.on('exit', onTerminalExit);
-
     });
+    return deferred.promise;
+  }
+
+  grunt.registerMultiTask(taskName, taskDescription, function() {
+
+    q({
+      done: this.async(),
+      commands: [],
+      directories: this.data,
+      bin: path.resolve(__dirname, '../node_modules/imageoptim-cli/bin'),
+      exec: q.denodeify(require('child_process').exec),
+      options: this.options({
+        jpegMini: false,
+        imageAlpha: false,
+        quitAfter: false
+      })
+    })
+
+    .then(function(config) {
+      if (!(config.directories instanceof Array)) {
+        throw new Error('config.files is not an array');
+      }
+      return config;
+    })
+
+    // convert all paths to absolute.
+    // convert paths such as '~/Desktop/directory' which contain tildes.
+
+    .then(function(config) {
+      config.directories = config.directories.map(function(route) {
+        if (route.substr(0, 1) === '~') {
+          route = process.env.HOME + route.substr(1);
+        }
+        return path.resolve(route);
+      });
+      return config;
+    })
+
+    // ensure all paths resolve properly to a file system entity
+
+    .then(function(config) {
+      return q.all(config.directories.map(fileExists))
+        .then(function() {
+          return config;
+        });
+    })
+
+    // ensure all paths are directories
+
+    .then(function(config) {
+      return q.all(config.directories.map(function(directory) {
+        return stat(directory);
+      }))
+        .then(function(statObjects) {
+          statObjects.forEach(function(statObject, i) {
+            var error = '';
+            if (statObject.isDirectory()) {
+              return;
+            }
+            error += 'Psssst! ImageOptim-CLI uses folders.\n';
+            error += 'Pass the folder containing "' + config.directories[i] + '"';
+            error += ' instead and we should be good to go.';
+            throw new Error(error);
+          });
+          return config;
+        });
+    })
+
+    // ensure the ImageOptim-CLI binary is accessible
+
+    .then(function(config) {
+      return fileExists(config.bin)
+        .then(function() {
+          return config;
+        })
+        .fail(function() {
+          var error = '';
+          error += 'Unable to locate ImageOptim-CLI in "' + config.bin + '".\n';
+          error += 'Please raise issue: https://github.com/JamieMason/grunt-imageoptim/issues/new.';
+          throw new Error(error);
+        });
+    })
+
+    // now we have valid paths to directories, we can construct our terminal commands
+
+    .then(function(config) {
+      config.commands = config.directories.map(function(directory) {
+        var command = './imageOptim';
+        var options = config.options;
+        command += options.quitAfter ? ' --quit' : '';
+        command += options.imageAlpha ? ' --image-alpha' : '';
+        command += options.jpegMini ? ' --jpeg-mini' : '';
+        return command + ' --directory ' + directory.replace(/\s/g, '\\ ');
+      });
+      return config;
+    })
+
+    // run our terminal commands
+
+    .then(function(config) {
+      return q.all(config.commands.map(function(command) {
+        return config.exec(command, {
+          cwd: config.bin
+        });
+      })).then(function() {
+        return config;
+      });
+    })
+
+    // here everything worked out and we can tell Grunt we're done
+
+    .then(function(config) {
+      config.done();
+    })
+
+    // here something failed, so we let Grunt render our errors to the console
+
+    .done();
 
   });
 
