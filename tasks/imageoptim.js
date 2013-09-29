85574270f1ed804ebc6bcb956a13db3a839e8b25
Jamie Mason <siunmaru@gmail.com>
2013-09-29 16:29:44 +0100

    add support for Grunt's file pattern matching
    process matches which are directories. files to follow. refs #9

--- tasks/imageoptim.js
+++ tasks/imageoptim.js
@@ -31,42 +31,56 @@ module.exports = function(grunt) {
 
   var q = require('q');
   var path = require('path');
-  var fs = require('fs');
-  var stat = q.denodeify(fs.stat);
+  var exec = require('child_process').exec;
   var taskName = 'imageoptim';
+  var issuesPage = 'https://github.com/JamieMason/grunt-imageoptim/issues/new';
   var taskDescription = 'Losslessly compress images from the command line';
-  var defaultOptions = {
-    jpegMini: false,
-    imageAlpha: false,
-    quitAfter: false
-  };
+  var cliPaths = [
+    '../node_modules/imageoptim-cli/bin',
+    '../../imageoptim-cli/bin'
+  ].map(function(dir) {
+    return path.resolve(__dirname, dir);
+  });
 
-  function fileExists(filePath) {
-    var deferred = q.defer();
-    fs.exists(filePath, function(canLocate) {
-      if (canLocate) {
-        deferred.resolve(true);
-      } else {
-        deferred.reject(new Error('"' + filePath + '" does not exist'));
-      }
-    });
-    return deferred.promise;
+  /**
+   * Get the ImageOptim-CLI Terminal command to be run for a given directory and task options
+   * @param  {String} directory
+   * @param  {Object} options
+   * @return {String}
+   */
+
+  function getCommandByDirectory(directory, options) {
+    var command = 'echo ./imageOptim';
+    command += options.quitAfter ? ' --quit' : '';
+    command += options.imageAlpha ? ' --image-alpha' : '';
+    command += options.jpegMini ? ' --jpeg-mini' : '';
+    return command + ' --directory ' + directory.replace(/\s/g, '\\ ');
   }
 
   /**
-   * Ensure inner is an Object containing all the members of outer
-   * @param  {Object} inner
-   * @param  {Object} outer
-   * @return {Object}
+   * @param  {String} command
+   * @param  {String} cwd
+   * @return {Promise}
    */
 
-  function mergeOptions(inner, outer) {
-    return Object.keys(outer).reduce(function(memo, optionName) {
-      if ((optionName in memo) === false) {
-        memo[optionName] = outer[optionName];
-      }
-      return memo;
-    }, inner || {});
+  function executeDirectoryCommand(command, cwd) {
+
+    var deferred = q.defer();
+    var errorMessage = 'ImageOptim-CLI exited with a failure status';
+    var imageOptimCli = exec(command, {
+      cwd: cwd
+    });
+
+    imageOptimCli.stdout.on('data', function(message) {
+      console.log((message || '').replace(/\n+$/, ''));
+    });
+
+    imageOptimCli.on('exit', function(code) {
+      return code === 0 ? deferred.resolve(true) : deferred.reject(new Error(errorMessage));
+    });
+
+    return deferred.promise;
+
   }
 
   /**
@@ -74,197 +88,82 @@ module.exports = function(grunt) {
    * @param  {Boolean}   opts.jpegMini     Whether to run JPEGmini.app.
    * @param  {Boolean}   opts.imageAlpha   Whether to run ImageAlpha.app.
    * @param  {Boolean}   opts.quitAfter    Whether to quit apps after running.
-   * @param  {Function}  done
    * @return {Promise}
    */
 
-  function processItem(files, opts, done) {
-
-    return q({
-      done: done,
-      commandOptions: {},
-      commands: [],
-      directories: files,
-      bin: [
-        path.resolve(__dirname, '../node_modules/imageoptim-cli/bin'),
-        path.resolve(__dirname, '../../imageoptim-cli/bin')
-      ],
-      exec: require('child_process').exec,
-      options: opts
-    })
-
-    .then(function(config) {
-      if (!(config.directories instanceof Array)) {
-        throw new Error('config.files is not an array');
-      }
-      return config;
-    })
-
-    // convert all paths to absolute.
-    // convert paths such as '~/Desktop/directory' which contain tildes.
-
-    .then(function(config) {
-      config.directories = config.directories.map(function(route) {
-        if (route.substr(0, 1) === '~') {
-          route = process.env.HOME + route.substr(1);
-        }
-        return path.resolve(route);
+  function processDirectories(files, opts) {
+    return files.map(function(directory) {
+      return getCommandByDirectory(directory, opts);
+    }).reduce(function(promise, command) {
+      return promise.then(function() {
+        return executeDirectoryCommand(command, opts.cliPath);
       });
-      return config;
-    })
-
-    // ensure all paths resolve properly to a file system entity
-
-    .then(function(config) {
-      return q.all(config.directories.map(fileExists))
-        .then(function() {
-          return config;
-        });
-    })
-
-    // ensure all paths are directories
-
-    .then(function(config) {
-      return q.all(config.directories.map(function(directory) {
-        return stat(directory);
-      }))
-        .then(function(statObjects) {
-          statObjects.forEach(function(statObject, i) {
-            var error = '';
-            if (statObject.isDirectory()) {
-              return;
-            }
-            error += 'Psssst! ImageOptim-CLI supports processing custom batches of files, but ';
-            error += 'grunt-imageoptim currently only supports folders.\n';
-            error += 'Pass the folder containing "' + config.directories[i] + '"';
-            error += ' instead and we should be good to go.';
-            throw new Error(error);
-          });
-          return config;
-        });
-    })
-
-    // ensure the ImageOptim-CLI binary is accessible
-
-    .then(function(config) {
-      var childBin = config.bin[0];
-      var siblingBin = config.bin[1];
-      return fileExists(childBin)
-        .then(function() {
-          config.bin = childBin;
-          return config;
-        })
-        .fail(function() {
-          return fileExists(siblingBin)
-            .then(function() {
-              config.bin = siblingBin;
-              return config;
-            })
-            .fail(function() {
-              var error = '';
-              error += 'Unable to locate ImageOptim-CLI in ';
-              error += '"' + childBin + '" or "' + siblingBin + '".\n';
-              error += 'Please raise issue: https://github.com/JamieMason/grunt-imageoptim/issues/new.';
-              throw new Error(error);
-            });
-        });
-    })
-
-    // now we have valid paths to directories, we can construct our terminal commands
-
-    .then(function(config) {
-      config.commands = config.directories.map(function(directory) {
-        var command = './imageOptim';
-        var options = config.options;
-        command += options.quitAfter ? ' --quit' : '';
-        command += options.imageAlpha ? ' --image-alpha' : '';
-        command += options.jpegMini ? ' --jpeg-mini' : '';
-        return command + ' --directory ' + directory.replace(/\s/g, '\\ ');
-      });
-      config.commandOptions.cwd = config.bin;
-      return config;
-    })
-
-    // run our terminal commands
-
-    .then(function(config) {
-
-      // we have to run each task serially
-      var allTasks = config.commands.reduce(function(promise, command) {
-
-        return promise.then(function() {
-
-          var deferred = q.defer();
-          var imageOptimCli = config.exec(command, config.commandOptions);
-
-          // surface all console output from ImageOptim-CLI
-          imageOptimCli.stdout.on('data', function(message) {
-            message = message || '';
-            console.log(message.replace(/\n+$/, ''));
-          });
-
-          // surface ImageOptim-CLI's success/fail status
-          imageOptimCli.on('exit', function(code) {
-            return code === 0 ? deferred.resolve(config) : deferred.reject(new Error());
-          });
-
-          return deferred.promise;
-
-        });
-
-      }, q(config));
-
-      return allTasks.then(function() {
-        return config;
-      });
-
-    })
-
-    // here everything worked out and we can tell Grunt we're done
-
-    .then(function(config) {
-      config.done();
-    })
-
-    // here something failed, so we let Grunt render our errors to the console
-
-    .done();
-
+    }, q());
   }
 
-  grunt.registerTask(taskName, taskDescription, function() {
+  /**
+   * @param  {String} str "hello"
+   * @return {String}     "Hello"
+   */
 
-    var done = this.async();
-    var config = grunt.config.data.imageoptim;
-    var taskNames = Object.keys(config);
-    var baseOptions = mergeOptions(config.options, defaultOptions);
+  function firstUp(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
 
-    if ('files' in config) {
-      return processItem(config.files, baseOptions, done);
+  /**
+   * @param  {String}  fileType "file" or "Dir"
+   * @return {Function}
+   */
+
+  function isFileType(fileType) {
+    var methodName = 'is' + firstUp(fileType);
+    return function(file) {
+      return grunt.file[methodName](file);
+    };
+  }
+
+  /**
+   * Ensure the ImageOptim-CLI binary is accessible
+   * @return {String}
+   */
+
+  function getPathToCli() {
+    return cliPaths.filter(function(cliPath) {
+      return grunt.file.exists(cliPath);
+    })[0];
+  }
+
+  grunt.registerMultiTask(taskName, taskDescription, function() {
+
+    var task = this;
+    var done = task.async();
+    var promise = q();
+    var cliPath = getPathToCli();
+    var options = task.options({
+      jpegMini: false,
+      imageAlpha: false,
+      quitAfter: false
+    });
+
+    if (!cliPath) {
+      throw new Error('Unable to locate ImageOptim-CLI. Please raise issue at ' + issuesPage);
     }
 
-    // @TODO: This can probably be improved with promises and taskNames.reduce
-
-    function nextTask() {
-
-      var taskName = taskNames.shift();
-      var task;
-
-      if (!taskName) {
-        return done();
+    task.files.forEach(function(file) {
+      var directories = file.src.filter(isFileType('dir'));
+      if (directories.length > 0) {
+        promise = promise.then(function() {
+          return processDirectories(directories, {
+            cliPath: cliPath,
+            jpegMini: options.jpegMini,
+            imageAlpha: options.imageAlpha,
+            quitAfter: options.quitAfter
+          });
+        });
       }
+    });
 
-      if (taskName === 'options') {
-        return nextTask();
-      }
-
-      grunt.log.writeln('  "imageoptim:' + taskName + '"');
-      task = config[taskName];
-      processItem(task.files, mergeOptions(task.options, baseOptions), nextTask);
-
-    }
-
-    nextTask();
+    promise.done(done);
 
   });
 
