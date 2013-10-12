70cdc6fac904ab7393cc6c84b697ec33ea8615c6
Jamie Mason <siunmaru@gmail.com>
2013-10-12 17:01:15 +0200

    remove local test config

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,38 +18,7 @@ module.exports = function(grunt) {
       files: {
         src: ['tasks/**/*.js']
       }
-    },
-    // imageoptim: {
-    //   options: {
-    //     jpegMini: false,
-    //     imageAlpha: false,
-    //     quitAfter: false
-    //   },
-    //   someTask: {
-    //     files: [
-    //       '/Users/jdog/Desktop/img',
-    //       '/Users/jdog/Desktop/img/*.jpg',
-    //       '/Users/jdog/Desktop/img/NOT_FOUND.tiff',
-    //       '/Users/jdog/Desktop/img/*.png',
-    //       '/Users/jdog/Desktop/img/*.gif'
-    //     ],
-    //     options: {
-    //       jpegMini: true,
-    //       imageAlpha: true,
-    //       quitAfter: true
-    //     }
-    //   },
-    //   otherTask: {
-    //     src: [
-    //       '/Users/jdog/Desktop/img2'
-    //     ],
-    //     options: {
-    //       jpegMini: false,
-    //       imageAlpha: false,
-    //       quitAfter: true
-    //     }
-    //   }
-    // }
+    }
   });
 
   grunt.loadTasks('tasks');
